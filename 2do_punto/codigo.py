8295b5fae7778cfb2618afcff1ef5f48b0037619
Mateo <pateguerrero@gmail.com>
2023-03-02 16:27:51 -0500

    Update
    Se cambio la forma de graficar el árbol

--- 2do_punto/codigo.py
+++ 2do_punto/codigo.py
@@ -1,6 +1,6 @@
-from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.datasets import load_iris
-import graphviz
+import matplotlib.pyplot as plt
 
 # Cargar el conjunto de datos de iris
 iris = load_iris()
@@ -11,13 +11,7 @@ clf = DecisionTreeClassifier()
 # Entrenar el clasificador en el conjunto de entrenamiento
 clf.fit(iris.data, iris.target)
 
-# Exportar el árbol a formato DOT
-dot_data = export_graphviz(clf, out_file=None, 
-                           feature_names=iris.feature_names,  
-                           class_names=iris.target_names,  
-                           filled=True, rounded=True,  
-                           special_characters=True)
-
 # Visualizar el árbol
-graph = graphviz.Source(dot_data)  
-graph.view()
+fig, ax = plt.subplots(figsize=(12, 12))
+plot_tree(clf, filled=True, feature_names=iris.feature_names, class_names=iris.target_names, ax=ax)
+plt.show()
