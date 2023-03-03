356c904c9c93eacc00055119e152b4a9ae1feda8
BrayanGuti <99362850+BrayanGuti@users.noreply.github.com>
2023-03-02 22:34:59 -0500

    elpepe

---
+++ 3er_punto/arbolB.py
@@ -0,0 +1,8 @@
+from lepepee import AVLTree
+
+arbol = AVLTree()
+arbol.insert_node(arbol.root, 12)
+arbol.insert_node(arbol.root, 11)
+arbol.insert_node(arbol.root, 13)
+arbol.insert_node(arbol.root, 1)
+arbol.delete_node(arbol.root, 3)
--- 3er_punto/lepepee.py
+++ 3er_punto/lepepee.py
@@ -1,144 +1,139 @@
-class AVLNode:
-    def __init__(self, key):
-        self.key = key
-        self.left = None
-        self.right = None
-        self.height = 1
+from Node import AVLNode
 
 class AVLTree:
     def __init__(self):
         self.root = None
 
-def get_height(node):
-    if node is None:
-        return 0
-    return node.height
+    def get_height(self, node: AVLNode):
+        if node is None:
+            return 0
+        return node.height
 
-def get_balance(node):
-    if node is None:
-        return 0
-    return get_height(node.left) - get_height(node.right)
+    def get_balance(self, node: AVLNode):
+        if node is None:
+            return 0
+        return self.get_height(node.left) - self.get_height(node.right)
 
-def rotate_right(node):
-    left_node = node.left
-    right_of_left = left_node.right
+    def rotate_right(self, node: AVLNode):
+        left_node = node.left
+        right_of_left = left_node.right
 
-    left_node.right = node
-    node.left = right_of_left
+        left_node.right = node
+        node.left = right_of_left
 
-    node.height = max(get_height(node.left), get_height(node.right)) + 1
-    left_node.height = max(get_height(left_node.left), get_height(left_node.right)) + 1
+        node.height = max(self.get_height(node.left), self.get_height(node.right)) + 1
+        left_node.height = max(self.get_height(left_node.left), self.get_height(left_node.right)) + 1
 
-    return left_node
+        return left_node
 
-def rotate_left(node):
-    right_node = node.right
-    left_of_right = right_node.left
+    def rotate_left(self, node: AVLNode):
+        right_node = node.right
+        left_of_right = right_node.left
 
-    right_node.left = node
-    node.right = left_of_right
+        right_node.left = node
+        node.right = left_of_right
 
-    node.height = max(get_height(node.left), get_height(node.right)) + 1
-    right_node.height = max(get_height(right_node.left), get_height(right_node.right)) + 1
+        node.height = max(self.get_height(node.left), self.get_height(node.right)) + 1
+        right_node.height = max(self.get_height(right_node.left), self.get_height(right_node.right)) + 1
 
-    return right_node
+        return right_node
 
-def insert_node(node, key):
-    if node is None:
-        return AVLNode(key)
-    elif key < node.key:
-        node.left = insert_node(node.left, key)
-    else:
-        node.right = insert_node(node.right, key)
+    def insert_node(self, node: AVLNode, key):
+        if node is None:
+            self.root = AVLNode(key)
+        elif key < node.key:
+            node.left = self.insert_node(node.left, key)
+        else:
+            node.right = self.insert_node(node.right, key)
 
-    node.height = max(get_height(node.left), get_height(node.right)) + 1
+        node.height = max(self.get_height(node.left), self.get_height(node.right)) + 1
 
-    balance = get_balance(node)
+        balance = self.get_balance(node)
 
-    if balance > 1 and key < node.left.key:
-        return rotate_right(node)
+        if balance > 1 and key < node.left.key:
+            return self.rotate_right(node)
 
-    if balance < -1 and key > node.right.key:
-        return rotate_left(node)
+        if balance < -1 and key > node.right.key:
+            return self.rotate_left(node)
 
-    if balance > 1 and key > node.left.key:
-        node.left = rotate_left(node.left)
-        return rotate_right(node)
+        if balance > 1 and key > node.left.key:
+            node.left = self.rotate_left(node.left)
+            return self.rotate_right(node)
 
-    if balance < -1 and key < node.right.key:
-        node.right = rotate_right(node.right)
-        return rotate_left(node)
+        if balance < -1 and key < node.right.key:
+            node.right = self.rotate_right(node.right)
+            return self.rotate_left(node)
 
-    return node
-
-def get_min_value_node(node):
-    current = node
-
-    # Find the leftmost leaf node
-    while current.left is not None:
-        current = current.left
-
-    return current
-
-
-def delete_node(node, key):
-    if node is None:
         return node
 
-    elif key < node.key:
-        node.left = delete_node(node.left, key)
+    def get_min_value_node(self, node: AVLNode):
+        current = node
 
-    elif key > node.key:
-        node.right = delete_node(node.right, key)
+        # Find the leftmost leaf node
+        while current.left is not None:
+            current = current.left
 
-    else:
-        # Node has one or no children
-        if node.left is None:
-            temp = node.right
-            node = None
-            return temp
+        return current
 
-        elif node.right is None:
-            temp = node.left
-            node = None
-            return temp
 
-        # Node has two children, get the inorder successor
-        temp = get_min_value_node(node.right)
+    def delete_node(self, node: AVLNode, key):
+        if node is None:
+            return node
 
-        # Copy the inorder successor's value to this node
-        node.key = temp.key
+        elif key < node.key:
+            node.left = self.delete_node(node.left, key)
 
-        # Delete the inorder successor
-        node.right = delete_node(node.right, temp.key)
+        elif key > node.key:
+            node.right = self.delete_node(node.right, key)
 
-    # If the tree has only one node, return
-    if node is None:
+        else:
+            # Node has one or no children
+            if node.left is None:
+                temp = node.right
+                node = None
+                return temp
+
+            elif node.right is None:
+                temp = node.left
+                node = None
+                return temp
+
+            # Node has two children, get the inorder successor
+            temp = self.get_min_value_node(node.right)
+
+            # Copy the inorder successor's value to this node
+            node.key = temp.key
+
+            # Delete the inorder successor
+            node.right = self.delete_node(node.right, temp.key)
+
+        # If the tree has only one node, return
+        if node is None:
+            return node
+
+        # Update height of the current node
+        node.height = 1 + max(self.get_height(node.left),
+                            self.get_height(node.right))
+
+        # Get the balance factor of this node
+        balance = self.get_balance(node)
+
+        # Left Left Case
+        if balance > 1 and self.get_balance(node.left) >= 0:
+            return self.rotate_right(node)
+
+        # Left Right Case
+        if balance > 1 and self.get_balance(node.left) < 0:
+            node.left = self.rotate_left(node.left)
+            return self.rotate_right(node)
+
+        # Right Right Case
+        if balance < -1 and self.get_balance(node.right) <= 0:
+            return self.rotate_left(node)
+
+        # Right Left Case
+        if balance < -1 and self.get_balance(node.right) > 0:
+            node.right = self.rotate_right(node.right)
+            return self.rotate_left(node)
+
         return node
-
-    # Update height of the current node
-    node.height = 1 + max(get_height(node.left),
-                          get_height(node.right))
-
-    # Get the balance factor of this node
-    balance = get_balance(node)
-
-    # Left Left Case
-    if balance > 1 and get_balance(node.left) >= 0:
-        return rotate_right(node)
-
-    # Left Right Case
-    if balance > 1 and get_balance(node.left) < 0:
-        node.left = rotate_left(node.left)
-        return rotate_right(node)
-
-    # Right Right Case
-    if balance < -1 and get_balance(node.right) <= 0:
-        return rotate_left(node)
-
-    # Right Left Case
-    if balance < -1 and get_balance(node.right) > 0:
-        node.right = rotate_right(node.right)
-        return rotate_left(node)
-
-    return node
