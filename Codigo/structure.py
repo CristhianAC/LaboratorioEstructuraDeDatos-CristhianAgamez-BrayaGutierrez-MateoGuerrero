776875c8ffcebf3184728b75d11da512f780ef68
Cristhian Agamez <93165832+CristhianAC@users.noreply.github.com>
2023-03-03 00:56:05 -0500

    Web

--- Codigo/structure.py
+++ Codigo/structure.py
@@ -5,10 +5,16 @@ import dash_core_components as dcc
 class htmlwriter():
     def __init__(self) -> None:
         
-        self.app = dash.Dash()
+        self.app = dash.Dash(__name__)
         graf = graficador()
         self.fig=graf.generador()
-        self.fig.show()
-    def     
+        self.changer()
+    def changer(self):
+        
+        self.app.layout = html.Div([
+            html.H1("Arbol De Spotify", style={'textAlign' : 'center', 'align-items': 'center'}),
+            dcc.Graph(figure=self.fig,style={'height' : 800, 'width' : 1100, 'textAlign' : 'center'})
+        ])
+        self.app.run_server(debug= True, use_reloader=False)
         
 
